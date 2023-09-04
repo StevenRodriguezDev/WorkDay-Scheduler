8849c4881ce69457e95851f9a53dc6039a1f2782
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-09-03 23:11:15 -0600

    updated js

---
+++ Develop/script.js
@@ -0,0 +1,39 @@
+$(function () {
+  var tDate = $("#currentDay");
+
+  var date = dayjs().format("dddd, MMMM D, YYYY h:mm A");
+  tDate.text(date);
+
+  var currentHour = dayjs().format("H");
+  //
+  for (let i = 9; i < 18; i++) {
+    var timeBlock = $("#hour-" + i);
+
+    if (i < currentHour) {
+      timeBlock.addClass("past");
+    } else if (i == currentHour) {
+      timeBlock.addClass("present");
+    } else {
+      timeBlock.addClass("future");
+    }
+  }
+  //variable for the button element
+  var SaveItem = $(".saveBtn");
+  var thingsToDo = JSON.parse(localStorage.getItem("events")) || {};
+
+  SaveItem.on("click", storeEvents);
+  for (const key in thingsToDo) {
+    var timeBlockB = $("#" + key).children()[1];
+    timeBlockB.value = thingsToDo[key];
+    console.log(timeBlockB);
+  }
+  function storeEvents(event) {
+    var eventStore = {
+      event: $(this).siblings()[1].value,
+      time: $(this).parent().attr("id"),
+    };
+    thingsToDo[eventStore.time] - eventStore.event;
+
+    localStorage.setItem("events", JSON.stringify(thingsToDo));
+  }
+});
